44f0ddf78964edc587806e239258bf6639d3ebb7
Pierre Koebelin <pierre.koebelin@skan.ch>
2025-01-06 15:33:45 +0100

    Enable spell check for markdown and text files

--- lua/options.lua
+++ lua/options.lua
@@ -82,3 +82,10 @@ if vim.fn.has("termguicolors") then
 end
 
 vim.opt.exrc = true
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "markdown", "text", "gitcommit" },
+    callback = function()
+        vim.opt_local.spell = true
+        vim.opt_local.spelllang = "en_us"
+    end,
+})
