6a89b806c781e7414331044975e35946656e9431
Pierre Koebelin <pierre.koebelin@skan.ch>
2025-01-06 10:28:47 +0100

    Disable word wrap for markdown files

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -18,24 +18,8 @@ return {
             vim.cmd([[do FileType]])
         end,
     },
-    -- {
-    --     "iamcco/markdown-preview.nvim",
-    --     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    --     build = "cd app && yarn install",
-    --     init = function()
-    --         vim.g.mkdp_filetypes = { "markdown" }
-    --     end,
-    --     ft = { "markdown" },
-    --     keys = {
-    --         {
-    --             "<leader>mp",
-    --             ft = "markdown",
-    --             "<cmd>MarkdownPreviewToggle<cr>",
-    --             desc = "Markdown Preview",
-    --         },
-    --     },
-    -- },
     {
         "Myzel394/easytables.nvim",
+        opts = {},
     },
 }
